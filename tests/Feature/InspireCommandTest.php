25efaa2170b92e2589e71fbc364e0147bfa403dc
Nick Jansen <n.jansen@sitepilot.io>
2023-03-13 14:40:19 +0100

    Update tests

--- tests/Feature/InspireCommandTest.php
+++ tests/Feature/InspireCommandTest.php
@@ -1,5 +1,5 @@
 <?php
 
-it('inspires artisans', function () {
-    $this->artisan('inspire')->assertExitCode(0);
+it('creates a configuration file', function () {
+    $this->artisan('init')->assertExitCode(0);
 });
