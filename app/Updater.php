d4716c623f2327e0a4aef28d19b4a23586a939dd
Nick Jansen <n.jansen@sitepilot.io>
2023-03-13 15:38:26 +0100

    Update version

--- app/Updater.php
+++ app/Updater.php
@@ -3,17 +3,12 @@
 namespace App;
 
 use Humbug\SelfUpdate\Strategy\GithubStrategy;
-use Illuminate\Support\Str;
 use LaravelZero\Framework\Components\Updater\Strategy\StrategyInterface;
 
 class Updater extends GithubStrategy implements StrategyInterface
 {
     public function __construct()
     {
-        $packageName = Str::slug(config('app.name'));
-
-        $this->setPackageName($packageName);
-        $this->setCurrentLocalVersion(config('app.version'));
-        $this->setPharName($packageName);
+        $this->setPharName('flight');
     }
 }
