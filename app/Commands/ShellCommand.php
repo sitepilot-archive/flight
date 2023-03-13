a05b94ddd838dedfe9e30e26a16b578f6dbd8bd5
Nick Jansen <n.jansen@sitepilot.io>
2023-03-13 15:23:51 +0100

    Catch exception

--- app/Commands/ShellCommand.php
+++ app/Commands/ShellCommand.php
@@ -2,6 +2,8 @@
 
 namespace App\Commands;
 
+use Exception;
+
 class ShellCommand extends Command
 {
     protected $signature = 'shell {--host}';
@@ -10,13 +12,17 @@ class ShellCommand extends Command
 
     public function handle(): void
     {
-        if (
-            !$this->option('host')
-            && $this->shouldRunInContainer()
-        ) {
-            $this->composeCmd([$this->config->get('container.shell', 'bash')])->mustRun();
-        } else {
-            $this->remoteCmd([$this->config->get('remote.shell', 'bash')])->run();
+        try {
+            if (
+                !$this->option('host')
+                && $this->shouldRunInContainer()
+            ) {
+                $this->composeCmd([$this->config->get('container.shell', 'bash')])->mustRun();
+            } else {
+                $this->remoteCmd([$this->config->get('remote.shell', 'bash')])->run();
+            }
+        } catch (Exception) {
+            //
         }
     }
 }
