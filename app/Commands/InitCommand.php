72e3e02ce97849bbb30d82b360e28a2ac677a44e
Nick Jansen <n.jansen@sitepilot.io>
2023-06-21 11:12:06 +0200

    Update tests

--- app/Commands/InitCommand.php
+++ app/Commands/InitCommand.php
@@ -15,28 +15,26 @@ class InitCommand extends Command
     {
         $file = getcwd() . DIRECTORY_SEPARATOR . 'flight.yml';
 
-        $host = $this->ask('Remote host');
-        $port = $this->ask('Remote port', 22);
-        $user = $this->ask('Remote user', 'captain');
-        $path = $this->ask('Remote path', '~/code/' . basename(getcwd()));
-
         if (!File::exists($file)) {
+            $config = [
+                'host' => $this->ask('Remote project host'),
+                'port' => (int)$this->ask('Remote SSH port', 22),
+                'user' => $this->ask('Remote SSH user', 'captain'),
+                'path' => $this->ask('Remote project path', '~/code/' . basename(getcwd()))
+            ];
+
             File::put($file,
-                Yaml::dump([
-                    'host' => $host,
-                    'port' => (int)$port,
-                    'user' => $user,
-                    'path' => $path,
+                Yaml::dump(array_merge($config, [
                     'sync' => [
                         'ignore' => [
                             '.idea', '.fleet', '.vscode', '.DS_Store', 'flight.yml', 'node_modules'
                         ]
                     ],
-                ], 99, 2));
+                ]), 99, 2));
 
             $this->info("Flight configuration initialized!");
         } else {
-            $this->abort('A flight configuration file already exists.');
+            $this->abort('Flight configuration file already exists.');
         }
     }
 }
